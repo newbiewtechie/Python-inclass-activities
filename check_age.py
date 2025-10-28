3d13ae7048afd205bacf84e179553aec062f1505
newbiewtechie <devanshinigam.codingal@gmail.com>
2025-10-28 19:34:48 +0530

    M2ACP1 added

---
+++ check_age.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age>= 10 :
+    if age <= 20:
+        print("Your age is between 10- 20 years, so it's a valid age")
+    else:
+        print("Yourage is above 20, Kindly enter a valid age, between 10- 20 years" )
+else:
+    print("Yourage is below 20 kindly enter a valid age between 1-020 years")
