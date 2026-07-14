6ca2b96271d4b064030cc83675d833f578ced4ab
newbiewtechie <devanshinigam.codingal@gmail.com>
2026-07-14 21:26:46 +0530

    M19L1A9

---
+++ break_activity.py
@@ -0,0 +1,9 @@
+word = str(input("Enter a word"))
+for i in word:
+    if i == 'a' or i == 'A' :
+        print("We got  a alphabet inside the word")
+        break
+    else:
+        print("a not found")
+
+    
