49d2c8cd6c3da60189ccf4fc4ce3c5695a0a0128
newbiewtechie <devanshinigam.codingal@gmail.com>
2026-07-14 21:19:04 +0530

    M19L1A6

---
+++ tip_waiter.py
@@ -0,0 +1,7 @@
+def total_calc(bill_amount, tip_perc):
+  total = bill_amount * (1+ 0.01 * tip_perc)
+  total= round(total,2)
+  print(f"Please pay Rs{total}")
+amount = float(input("Enter the bill amount"))
+tip= int(input("Enter the tip percentage "))
+total_calc(amount, tip)
