48b0d3da76ed23af1f81b62589124adc3ad662b1
newbiewtechie <devanshinigam.codingal@gmail.com>
2026-07-14 20:48:40 +0530

    M19ACP5

---
+++ mathsmodule.py
@@ -0,0 +1,6 @@
+import math
+num= int(input("Enter a number "))
+
+print("Sin of", num, "is:", int(math.sin(num)))
+print("Cos of", num, "is:", int(math.cos(num)))
+print("Tan of", num, "is:", int(math.tan(num)))
